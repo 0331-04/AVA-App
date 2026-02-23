94412e61eff531135c394b2fae1ce633e608f2d7
OmethG <ometh.20240437@iit.ac.lk>
2026-02-23 15:37:41 +0530

    Created Claims page
    This page has a Search Bar and can search all claims through claim id. Can view status and view details as well through this page.

--- Admin_frontend/src/App.jsx
+++ Admin_frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Splash from "./pages/Splash";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import ClaimDetails from "./pages/ClaimDetails";
+import Claims from "./pages/Claims";
 
 
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Splash />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/claims" element={<Claims />} />
         <Route path="/claims/:id" element={<ClaimDetails />} />
       </Routes>
     </BrowserRouter>
