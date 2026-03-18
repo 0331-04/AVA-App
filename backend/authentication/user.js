7e009eec6595c82cd844459695b544150c68ef41
DevujaSusmini <www.devuja.2004@gmail.com>
2026-03-19 00:19:35 +0530

    removed next parameter

--- backend/authentication/user.js
+++ backend/authentication/user.js
@@ -156,14 +156,13 @@ const UserSchema = new mongoose.Schema({
 // MIDDLEWARE
 
 // Hash password before saving
-UserSchema.pre('save', async function(next) {
+UserSchema.pre('save', async function() {
   if (!this.isModified('password')) {
-    next();
+    return;  
   }
   
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // INSTANCE METHODS
